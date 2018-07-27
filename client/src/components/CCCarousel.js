8468f270434cce050421fae9ad926582d3df53c9
Michelle Garcia <thelionpear@gmail.com>
2018-07-27 01:07:06 -0600

    carousel component

---
+++ client/src/components/CCCarousel.js
@@ -0,0 +1,67 @@
+import CC1 from '../images/cc1.jpg';
+import CC2 from '../images/cc2.jpg';
+import CC3 from '../images/cc3.jpg';
+import CC4 from '../images/cc4.jpg';
+import CC5 from '../images/cc5.jpg';
+import CC6 from '../images/cc6.jpg';
+import { render } from 'react-dom';
+import React, { Component } from 'react';
+import { Carousel, showArrows, onChange, onClickItem, onClickThumb, infiniteLoop } from 'react-responsive-carousel';
+import styles from 'react-responsive-carousel/lib/styles/carousel.min.css';
+import styled from 'styled-components';
+  
+class CCCarousel extends Component {
+  render() {
+      return (
+          <Carousel 
+            showArrows={true} 
+            onChange={onChange} 
+            onClickItem={onClickItem} 
+            onClickThumb={onClickThumb}
+            infiniteLoop={true}
+            autoPlay={true}
+            interval={3000}
+            width={700}
+            height={500}
+          >
+              <div>
+                  <img src={CC2} />
+              </div>
+              <div>
+                  <img src={CC3} />
+              </div>
+              <div>
+                  <img src={CC4} />
+              </div>
+              <div>
+                  <img src={CC5} />
+              </div>
+              <div>
+                  <img src={CC6} />
+              </div>
+          </Carousel>
+      );
+  }
+};
+
+
+<link rel="stylesheet" href="<NODE_MODULES_FOLDER>/react-responsive-carousel/lib/styles/carousel.min.css"/>
+    
+// const Div = styled.div `
+// text-align: center; 
+// align-items: center;
+// justify-content: center;
+// padding-top: 10px; 
+// padding-bottom: 10px; 
+// width: auto;
+// height: auto;
+// max-width: 400px;
+// max-height: 400px;
+// overflow: hidden; 
+// `
+
+// const CarouselBox = styled.div `
+
+// `
+
+export default CCCarousel 
--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -4,14 +4,12 @@ import { Container, Divider, Header, Menu, Image } from 'semantic-ui-react';
 import { Link, withRouter } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { handleLogout } from '../reducers/user';
-import Instagram from '../images/instagram_logo.png';
+import instaGram from '../images/instagram_logo.png';
 import twitter from '../images/twitter_logo.png';
 import faceBook from '../images/facebook_logo.png';
+import CCCarousel from './CCCarousel'; 
 
 class Home extends Component {
-
-
-  
 
   render() {
     return (
@@ -19,15 +17,18 @@ class Home extends Component {
         <UpperSection> 
           <Title as='h1' textAlign='center'>Cotton Candy Catering</Title>
           <Logos> 
-              <Image src={Instagram} size="mini" floated="right" circular alt="instagram logo"/> 
-              <span>Instagram</span> 
-              <Image src={faceBook}  size="mini" floated="right" circular alt="Facebook logo"/> 
-              <span>Facebook</span>
-              <Image src={twitter} size="mini" floated="right" circular alt="Twitter logo"/>
-              <span>Twitter</span> 
+              <a href="https://www.instagram.com/" target="_blank" rel="noopener noreferrer">
+                <Image src={instaGram} size="mini" floated="right" circular alt="instagram logo"/> 
+              </a> 
+              <a href="https://www.facebook.com/" target="_blank" rel="noopener noreferrer">
+                <Image src={faceBook}  size="mini" floated="right" circular alt="Facebook logo"/> 
+              </a>
+              <a href="https://twitter.com/" target="_blank" rel="noopener noreferrer"> 
+                <Image src={twitter} size="mini" floated="right" circular alt="Twitter logo"/>
+              </a> 
           </Logos> 
+          <Divider /> 
         </UpperSection> 
-        <Divider /> 
         <Container> 
           <GraySections> 
             <TitleText>A One-of-a-Kind Experience</TitleText> 
@@ -38,16 +39,19 @@ class Home extends Component {
               and your guests are sure to remember the event with fond memories! 
             </Text> 
           </GraySections> 
-          <GraySections> 
-            <Text> ***This is where the Carousel of images will go*** </Text> 
+          <Divider /> 
+          <GraySections>
+            <CCCarousel/> 
           </GraySections> 
+          <Divider /> 
           <GraySections> 
-            <Text>***This is where some fake reviews can go***</Text> 
+            <Text>***This is where some fake reviews can go, probably in three columns***</Text> 
           </GraySections> 
+          <Divider /> 
           <GraySections> 
             <TitleText>To Book Cotton Candy Catering for Your Event:</TitleText> 
             <Text>
-              Reach out to us by phone at: 555-555-555
+              Reach out to us by phone at: 555-555-5555
             </Text> 
             <Text>
               by email at: email@email.com 
@@ -59,14 +63,18 @@ class Home extends Component {
   }
 }
 
+
 const UpperSection = styled.div `
-margin-top: 40px; 
 flex-direction: column; 
 display: flex; 
+color: white; 
+background-color: gray; 
+padding-top: 40px; 
 `
 
 const Logos = styled.div `
 justify-content: flex-end; 
+margin-right: 15px; 
 `
 
 const TitleText = styled.h1 `
